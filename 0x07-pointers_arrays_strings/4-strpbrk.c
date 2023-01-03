3cca2fd8df73ba11cc5b3770808240dd965b3182
Nonnyman16 <nagod16@yahoo.com>
2023-01-03 14:57:16 -0800

    updated 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -15,12 +15,12 @@ char *_strpbrk(char *s, char *accept)
 		{
 			for (j = 0; *(accept + j); j++)
 			{
-				if (*(s + i) == *(accept +j))
+				if (*(s + i) == *(accept + j))
 				{
 					break;
 				}
 			}
-			if (*(accept +j) != '\0')
+			if (*(accept + j) != '\0')
 			{
 				return (s + i);
 			}
